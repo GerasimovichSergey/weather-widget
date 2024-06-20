6bfa4e5735c5092a7eb781ed8b2230d103f12f70
Gerasimovich Sergey <wmmoee@gmail.com>
2024-06-20 08:45:11 +0300

    add fucntion addZeroToTime

--- scripts/modules/utils.js
+++ scripts/modules/utils.js
@@ -1,3 +1,11 @@
+const addZeroToTime = (time) => {
+    if (time < 10) {
+        time = `0${time}`;
+    }
+
+    return time;
+};
+
 export const getCurrentDateTime = () => {
     const months = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
     const weekDays = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
@@ -8,16 +16,8 @@ export const getCurrentDateTime = () => {
     const month = months[date.getMonth()];
     const year = date.getFullYear();
     const dayOfWeek = weekDays[date.getDay()];
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-
-    if (hours < 10) {
-        hours = `0${hours}`;
-    }
-
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
+    let hours = addZeroToTime(date.getHours());
+    let minutes = addZeroToTime(date.getMinutes());
 
     return { dayOfMonth, month, year, dayOfWeek, hours, minutes };
 };
